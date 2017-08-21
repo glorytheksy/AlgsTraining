445982f63cc48797176387934ce32a55ac3960b5
glorythesky <yxmfight@hotmail.com>
2017-08-22 00:44:00 +0800

    汉诺塔 未完成

---
+++ src/main/java/stack_and_queue/HanoTower.java
@@ -0,0 +1,20 @@
+package stack_and_queue;
+
+import utils.Stack;
+
+public class HanoTower {
+
+    Stack A = new Stack();
+    Stack B = new Stack();
+    Stack C = new Stack();
+    
+    
+    public HanoTower(Stack A, Stack B, Stack C) {        
+        this.A = A;
+        this.B = B;
+        this.C = C;        
+    }
+    
+    
+    
+}
