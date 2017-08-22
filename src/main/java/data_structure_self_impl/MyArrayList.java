f2bc5854b2273af3f1242475be9c3bf47228eccd
glorythesky <yxmfight@hotmail.com>
2017-08-22 18:55:40 +0800

    asdf

---
+++ src/main/java/data_structure_self_impl/MyArrayList.java
@@ -0,0 +1,16 @@
+package data_structure_self_impl;
+
+public class MyArrayList<T> {
+    
+    private int size;
+    
+    private T [] list;
+    
+    
+    public MyArrayList() {
+        this.list = new T[0];
+    }
+    
+    
+
+}
