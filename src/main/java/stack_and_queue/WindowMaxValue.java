7d53501d5cbf0942e1671bc43995ae934c2eee7b
glorythesky <yxmfight@hotmail.com>
2017-08-25 14:01:04 +0800

    sss

--- src/main/java/stack_and_queue/WindowMaxValue.java
+++ src/main/java/stack_and_queue/WindowMaxValue.java
@@ -7,6 +7,14 @@ import java.util.List;
 public class WindowMaxValue {
     
     
+    
+    /**
+     * 构建一个双端队列 qmax。
+     * 遍历数组arr时，arr元素arr[i]的index i 放入队尾，
+     * 如果 arr[qmax[tail]] > arr[i], 则放入即可，不做其它动作；
+     * otherwise, qmax弹出队尾, 继续比较arr[i]与qmax“当前”队尾，直到遇到这样一个队尾使得 qmax[tail] > arr[i]
+     * 这样做可以保证 w 窗口向后移动的过程中，qmax始终存储着 w 窗口 后 w - 1 个元素的最大值位置信息 
+     */
     public List<Integer> solution(List<Integer> list, int w) {
         List<Integer> rs = new ArrayList<Integer>();
 
@@ -22,30 +30,30 @@ public class WindowMaxValue {
             return list;
         }
         
-        /**
-         * 构建一个双端队列 qmax。
-         * 遍历数组arr时，arr元素arr[i]的index i 放入队尾，
-         * 如果 arr[qmax[tail]] > arr[i], 则放入即可，不做其它动作；
-         * otherwise, qmax弹出队尾, 继续比较arr[i]与qmax“当前”队尾，直到遇到这样一个队尾使得 qmax[tail] > arr[i]
-         * 
-         * 
-         * 这样做可以保证 w 窗口向后移动的过程中，qmax始终存储着 w 窗口 前 w - 1 个元素的最大值位置信息 
-         */
-        
         LinkedList<Integer> qmax = new LinkedList<Integer>(); 
         for (int i = 0; i < w ; i++) {            
             if (0 == qmax.size()) {
                 qmax.add(i);
             } else {                                
-                while (list.get(qmax.getLast()) <= list.get(i) && qmax.size() > 0) {                    
+                while ( qmax.size() > 0 && list.get(qmax.getLast()) <= list.get(i)) {                    
                     qmax.removeLast();
                 }
                 qmax.add(i);   
             }            
         }
+        rs.add(list.get(qmax.getFirst()));
         
-        
-        return null;
+        for (int i = w ; i < list.size(); i++ ) {
+            if ( (w - 1)  < (i - qmax.getFirst()) ) {
+                qmax.removeFirst();                
+            }
+            while ( qmax.size() > 0 && list.get(qmax.getLast()) <= list.get(i) ) {                    
+                qmax.removeLast();
+            }
+            qmax.add(i);               
+            rs.add(list.get(qmax.getFirst()));
+        }                
+        return rs;
     }
 
 
---
+++ src/test/java/stack_and_queque/TestWindowMaxValue.java
@@ -0,0 +1,29 @@
+package stack_and_queque;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import stack_and_queue.WindowMaxValue;
+
+public class TestWindowMaxValue {
+
+    @Test
+    public void testSolution() {
+        
+        WindowMaxValue ww = new WindowMaxValue();                
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(4);
+        list.add(3);
+        list.add(5);
+        list.add(4);
+        list.add(3);
+        list.add(3);
+        list.add(6);
+        list.add(7);
+        
+        System.out.println(ww.solution(list, 3));
+    }
+    
+}
