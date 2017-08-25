33cbba7544f585ff609ad015dfdc9aba18898d1a
glorythesky <yxmfight@hotmail.com>
2017-08-25 09:14:44 +0800

    doing double-end queque

--- src/main/java/stack_and_queue/WindowMaxValue.java
+++ src/main/java/stack_and_queue/WindowMaxValue.java
@@ -1,94 +1,52 @@
 package stack_and_queue;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class WindowMaxValue {
     
     
     public List<Integer> solution(List<Integer> list, int w) {
+        List<Integer> rs = new ArrayList<Integer>();
+
         if (null == list) {
             throw new RuntimeException();
         }
-        
-        if (w > list.size() || w <= 0) {            
+
+        if (w > list.size() || w <= 0) {
             throw new RuntimeException();
         }
-        
+
         if (1 == w) {
             return list;
         }
         
-        List<Integer> rs = new ArrayList<Integer>();
+        /**
+         * 构建一个双端队列 qmax。
+         * 遍历数组arr时，arr元素arr[i]的index i 放入队尾，
+         * 如果 arr[qmax[tail]] > arr[i], 则放入即可，不做其它动作；
+         * otherwise, qmax弹出队尾, 继续比较arr[i]与qmax“当前”队尾，直到遇到这样一个队尾使得 qmax[tail] > arr[i]
+         * 
+         * 
+         * 这样做可以保证 w 窗口向后移动的过程中，qmax始终存储着 w 窗口 前 w - 1 个元素的最大值位置信息 
+         */
         
-        if (2 == w) {
-            int maxIndex = 0;
-            Integer maxValue = list.get(maxIndex);
-            for (int i = 0 ; i < list.size() - w + 1; i++) {
-                
-                
-            }                        
+        LinkedList<Integer> qmax = new LinkedList<Integer>(); 
+        for (int i = 0; i < w ; i++) {            
+            if (0 == qmax.size()) {
+                qmax.add(i);
+            } else {                                
+                while (list.get(qmax.getLast()) <= list.get(i) && qmax.size() > 0) {                    
+                    qmax.removeLast();
+                }
+                qmax.add(i);   
+            }            
         }
         
         
-        int begin = 0;                
-        int maxIndex = maxIndex(list.subList(1, w));;
-        int secondIndex = secondIndex(list.subList(1, w));                                
-        rs.add(list.get(maxIndex) > list.get(begin) ? list.get(maxIndex) : list.get(begin));
-        
-        
-        for (int i = 1; i < list.size() - w + 1; i++) {
-            if (begin == maxIndex) {
-                
-            }
-            
-            
-        }
+        return null;
     }
-    
-    
-    public int maxIndex(List<Integer> list) {        
-        if (null == list || 2 > list.size()) {            
-            return -1;            
-        }
-                
-        int maxIndex = (list.get(0) >  list.get(1) ? 0 : 1);
-        Integer maxValue = (list.get(0) >  list.get(1) ? list.get(0) : list.get(1));
-        int secondIndex = (maxIndex == 1 ? 0 : 1);
-        int secondValue = (maxIndex == 1 ? list.get(0) : list.get(1));
-        
-        for (int i = 0; i < list.size(); i++) {
-            if (maxValue < list.get(i)) {
-                secondValue = maxValue;
-                secondIndex = maxIndex;
-                
-                maxValue = list.get(i);
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
-    }
-    
-    public int secondIndex(List<Integer> list) {
-        if (null == list || 2 > list.size()) {            
-            return -1;            
-        }
-                
-        int maxIndex = (list.get(0) >  list.get(1) ? 0 : 1);
-        Integer maxValue = (list.get(0) >  list.get(1) ? list.get(0) : list.get(1));
-        int secondIndex = (maxIndex == 1 ? 0 : 1);
-        int secondValue = (maxIndex == 1 ? list.get(0) : list.get(1));
-        
-        for (int i = 0; i < list.size(); i++) {
-            if (maxValue < list.get(i)) {
-                secondValue = maxValue;
-                secondIndex = maxIndex;
-                
-                maxValue = list.get(i);
-                maxIndex = i;
-            }
-        }
-        return secondIndex;        
-    }
+
 
 }
