0af6467c857e65f5846a9f5b6943d483f8b06095
glorythesky <yxmfight@hotmail.com>
2017-08-26 15:38:29 +0800

    maxTree

---
+++ src/main/java/utils/BinaryNode.java
@@ -0,0 +1,20 @@
+package utils;
+
+
+public class BinaryNode {
+
+    public int value;
+    
+    public BinaryNode left;
+    
+    public BinaryNode right;
+    
+    
+    public BinaryNode() {
+    }
+    
+    public BinaryNode(int value) {
+        this.value = value;
+    }
+    
+}
