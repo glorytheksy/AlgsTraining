c929a817032532ecfe34740acfc8789215b7d437
glorythesky <yxmfight@hotmail.com>
2017-10-01 10:52:21 +0800

    合并两个有序单链表

---
+++ src/test/java/linked_nodes/TestMerge2Nodes.java
@@ -0,0 +1,24 @@
+package linked_nodes;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import utils.Node;
+
+public class TestMerge2Nodes extends LinkedNodesTestBase {
+    
+    Merge2Nodes alg = null;
+    
+    @Before
+    public void init() {
+        alg = new Merge2Nodes();
+    }
+    
+    @Test
+    public void testSolution() {
+        
+        Node rs = alg.solution(this.initNode("0,2,3,7"), this.initNode("1,3,5,7,9"));
+        this.printNodes(rs);
+    }
+    
+}
