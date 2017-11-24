dbc76012a81e10a6a26d99804ce42650d3180691
glorythesky <yxmfight@hotmail.com>
2017-11-24 18:34:26 +0800

    加入深度优先搜索算法

---
+++ src/main/java/impl_of_Algorithms/graph/DeepFirstSearch.java
@@ -0,0 +1,50 @@
+package impl_of_Algorithms.graph;
+
+/**
+ * 
+ * @ClassName DeepFirstSearch
+ * @Description 深度优先搜索
+ * @author 叶晓蒙 
+ * @Date 2017年11月24日 下午6:14:20
+ * @version 1.0.0
+ */
+public class DeepFirstSearch {
+
+    private MyGraph graph;
+    private boolean [] marked;
+    private int count;
+        
+    public DeepFirstSearch(MyGraph graph) {
+        if (null == graph) {
+            throw new RuntimeException("error");
+        }
+        this.graph = graph;
+        this.marked = new boolean [graph.getV()];
+        this.count = 0;
+    }
+    
+    public void dfs(int s) {
+        this.marked[s] = true;
+        this.count++;
+        
+        for (int e : this.graph.adj(s)) {
+            if (false == this.marked[e]) {
+                dfs(e);
+            }
+        }
+    }
+    
+    public int getCount() {
+        return this.count;
+    }
+    
+    public static void main(String[] args) {
+        MyGraph g = new MyGraph(2);
+        g.addEdge(0, 1);
+        DeepFirstSearch search = new DeepFirstSearch(g);
+        search.dfs(0);
+        System.out.println(search.getCount());
+    }
+    
+    
+}
---
+++ src/main/java/impl_of_Algorithms/graph/MyGraphSearch.java
@@ -0,0 +1,53 @@
+package impl_of_Algorithms.graph;
+
+/**
+ * 
+ * @ClassName MyGraphSearch
+ * @Description 无向图定点搜索类(专门用于某个无向图的某个点的查询)
+ * @author 叶晓蒙
+ * @Date 2017年11月24日 下午5:27:20
+ * @version 1.0.0
+ */
+public class MyGraphSearch {
+
+    private MyGraph graph;
+    private int v;
+    
+    public MyGraphSearch(MyGraph graph, int v) {
+        if (null == graph || v < 0) {
+            throw new RuntimeException("error");
+        }
+        this.graph = graph;
+        this.v = v;
+    }
+    
+    /**
+     * @Description v和s是否联通 
+     * @param s
+     * @return
+     */
+    public boolean marked(int s) {
+        Iterable<Integer> adjIter = this.graph.adj(v);
+        for (int e : adjIter) {
+            if (e == s) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * @Description v的度
+     * @return
+     */
+    public int count() {
+        Iterable<Integer> adjIter = this.graph.adj(v);
+        int count = 0;
+        for (int e : adjIter) {
+            count++;
+        }
+        return count;
+    }
+    
+    
+}
