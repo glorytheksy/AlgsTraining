5730225375de18cba021e39a0d3155c0a02cad60
glorythesky <yxmfight@hotmail.com>
2017-12-19 23:52:57 +0800

    ~~

---
+++ src/main/java/impl_of_Algorithms/graph/Edge.java
@@ -0,0 +1,58 @@
+package impl_of_Algorithms.graph;
+
+/**
+ * 
+ * @ClassName Edge
+ * @Description 权重边
+ * @author yxmfight
+ * @Date 2017年12月16日 下午8:36:56
+ * @version 1.0.0
+ */
+public class Edge implements Comparable<Edge> {
+
+    // 一个顶点
+    private int v1;
+    
+    // 另一个顶点
+    private int v2;
+    
+    // 权重
+    private double w;
+    
+    
+    public Edge(int v1, int v2, double weight) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.w = weight;
+    }
+    
+    public double weight() {
+        return this.w;
+    }
+    
+    public int either() {
+        return v1;
+    }
+    
+    public int other(int vertex) {
+        if (v1 == vertex) {
+            return v2;
+        } else if (v2 == vertex) {
+            return v1;
+        } else {
+            throw new RuntimeException("");
+        }
+    }
+    
+    public int compareTo(Edge e) {
+        if (e.weight() < this.weight()) {
+            return 1;
+        } else if (e.weight() > this.weight()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+
---
+++ src/main/java/impl_of_Algorithms/graph/EdgeWeightedGraph.java
@@ -0,0 +1,55 @@
+package impl_of_Algorithms.graph;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ * @ClassName EdgeWeightedGraph
+ * @Description 加权无向图
+ * @author 叶晓蒙
+ * @Date 2017年12月16日 下午8:48:00
+ * @version 1.0.0
+ */
+public class EdgeWeightedGraph {
+
+    private final int V;
+    
+    private int E;
+    
+    private HashSet<Edge>[] adj;
+    
+    public EdgeWeightedGraph(int V) {
+        this.V = V;
+        this.E = 0;
+        this.adj = (HashSet<Edge>[])new HashSet[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new HashSet<Edge>();
+        }
+    }
+    
+    public Iterable<Edge> Edges() {
+        Set<Edge> rs = new HashSet<Edge>();
+        for (int i = 0; i < V; i++) {
+            for (Edge e : this.adj(i) ) {
+                // 防止重复加边
+                if (e.other(i) > i) {
+                    rs.add(e);
+                }
+            }
+        }
+        return rs;
+    }
+    
+    public Iterable<Edge> adj(int v) {
+        return this.adj[v];
+    }
+    
+    public void addEdge(Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        this.adj[v].add(e);
+        this.adj[w].add(e);
+        E++;
+    }
+}
