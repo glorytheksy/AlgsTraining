84555e6fcb8077d39893d189e3c2e25142bc8651
glorythesky <yxmfight@hotmail.com>
2017-12-19 19:08:42 +0800

    ~~

--- src/main/java/data_structure_of_redis/JumpList.java
+++ src/main/java/data_structure_of_redis/JumpList.java
@@ -1,5 +1,8 @@
 package data_structure_of_redis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * @ClassName SkipList
@@ -8,9 +11,26 @@ package data_structure_of_redis;
  * @Date 2017年9月27日 上午9:11:47
  * @version 1.0.0
  */
-public class JumpList {
+public class JumpList<T> {
     
+    private List<Node<T>> list;
     
+    public JumpList() {
+        this.list = new ArrayList<Node<T>>();
+    }
     
+    public Node get(Node node) {
+        
+    }
+    
+    class Node<T> {
+        // 内容
+        T content;
+        // 下一个节点链接
+        Node next;
+        // 低一阶的对应节点链接
+        Node under;
+    }
 
 }
+
