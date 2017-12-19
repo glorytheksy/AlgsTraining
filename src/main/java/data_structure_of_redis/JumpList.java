b7957e2a274bed6128869cb735d1b0e75e0f6e61
glorythesky <yxmfight@hotmail.com>
2017-12-20 01:57:00 +0800

    ~~

--- src/main/java/data_structure_of_redis/JumpList.java
+++ src/main/java/data_structure_of_redis/JumpList.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @Date 2017年9月27日 上午9:11:47
  * @version 1.0.0
  */
-public class JumpList<T> {
+public class JumpList<T extends Comparable<T>> {
     
     private List<Node<T>> list;
     
@@ -19,17 +19,41 @@ public class JumpList<T> {
         this.list = new ArrayList<Node<T>>();
     }
     
+    @SuppressWarnings("unchecked")
     public Node get(Node node) {
-        return node;
+        if (list.isEmpty()) {
+            return null;
+        }
+         
+        Node<T> cur = list.get(list.size() - 1);
+        Node<T> upLimit = null;
+        for (int i = 0; i < list.size() - 1; i++ ) {
+            if (null == cur) {
+                return null;
+            }
+                        
+            if (cur.next.content.compareTo(node.next.content) < 0) {
+                cur = cur.next.under;
+            } else {
+                upLimit = cur.next.under;
+                cur = cur.under;
+                
+            }            
+        }
+        
+        
         
     }
     
-    class Node<T> {
+    @SuppressWarnings("hiding")
+    class Node<T extends Comparable<T>> {
         // 内容
         T content;
         // 下一个节点链接
+        @SuppressWarnings("rawtypes")
         Node next;
         // 低一阶的对应节点链接
+        @SuppressWarnings("rawtypes")
         Node under;
     }
 
