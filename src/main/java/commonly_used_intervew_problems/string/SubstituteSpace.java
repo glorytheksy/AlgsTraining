adde3d74440937717eaca477bf5000f393fd7efe
glorythesky <yxmfight@hotmail.com>
2018-06-08 17:34:52 +0800

    fix

---
+++ src/main/java/commonly_used_intervew_problems/string/SubstituteSpace.java
@@ -0,0 +1,28 @@
+package commonly_used_intervew_problems.string;
+
+/**
+ * @ClassName SubstituteSpace
+ * @Description substitute space(" ") with "%20" in a String
+ * @author 叶晓蒙
+ * @Date 2018年6月8日 下午4:54:39
+ * @version 1.0.0
+ */
+public class SubstituteSpace {
+    
+    public void solution(String str) {
+        int count = 0;
+        /* count how  many spaces are in the str */
+        for (int i = 0; i < str.length(); i++) {            
+            if (20 == str.charAt(i)) {
+                count++;
+            }
+        }                
+        
+        /* traverse the augmented str backwards, implement substitution */
+        str = str + 
+        for (int i = len ) {
+            
+        }
+    }
+    
+}
