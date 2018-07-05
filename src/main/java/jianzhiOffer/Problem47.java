7861a350c058fc37035d637773f902a7e842e577
glorythesky <yxmfight@hotmail.com>
2018-07-05 17:56:12 +0800

    fix

---
+++ src/main/java/jianzhiOffer/Problem47.java
@@ -0,0 +1,24 @@
+package jianzhiOffer;
+
+public class Problem47 {
+
+    public static int Add(int num1,int num2)
+    {
+        // 总和
+        int sum = 0;
+        // 进位
+        int carry = 0;
+        
+
+        do {
+            sum = num1 ^ num2;
+            carry = (num1&num2) <<1;
+            num1 = sum;
+            num2 = carry;
+        } while(num2!=0);
+
+
+        return num1;
+    }
+
+}
