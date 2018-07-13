d2057907cc6d3edcf8bae37ea664a26886c27046
glorythesky <yxmfight@hotmail.com>
2018-07-13 14:01:17 +0800

    fix

---
+++ src/main/java/jianzhiOffer/Permutation.java
@@ -0,0 +1,25 @@
+package jianzhiOffer;
+
+/**
+ * @ClassName Permutation
+ * @Description 
+ * @author yxmfight
+ * @Date 2018年7月13日 上午11:19:32
+ * @version 1.0.0
+ */
+public class Permutation {
+
+    public void solution(int [] a) {
+        
+        
+    }
+    
+    private void permutation(int [] a, int pos) {
+        
+        
+        
+        
+        
+    }
+            
+}
