280abffeab9bd99405d6994de404cc86202c87cf
glorythesky <yxmfight@hotmail.com>
2018-07-19 14:46:13 +0800

    fix

---
+++ src/main/java/jianzhiOffer/Problem41.java
@@ -0,0 +1,22 @@
+package jianzhiOffer;
+
+/**
+ * @ClassName Problem41
+ * @Description 输入一个递增排序的数组和一个数字s，
+ *              在数组中查找两个数，使得它们的和正好是s，
+ *              如果有多对数字的和等于s，输出任意一对即可。
+ * @author pc
+ * @Date 2018年7月19日 下午2:09:56
+ * @version 1.0.0
+ */
+public class Problem41 {
+
+    /**
+     * 首先想到的方法： 依次遍历，求出 s - x, 二分查找，时间复杂度 n * lgn
+     */
+    
+    /**
+     * 比较快的方法: 双指针法
+     */
+    
+}
