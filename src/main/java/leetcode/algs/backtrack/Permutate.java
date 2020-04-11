1412a3c08584720f73d75364d8198f9148774d0d
glorythesky <yxmfight@hotmail.com>
2020-04-11 15:22:17 +0800

    fix

--- src/main/java/leetcode/algs/backtrack/Permutate.java
+++ src/main/java/leetcode/algs/backtrack/Permutate.java
@@ -1,21 +1,11 @@
 package leetcode.algs.backtrack;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 
 public class Permutate {
-	
-	/**
-	 * 回溯法
-	 * @param nums
-	 * @return
-	 */
-	public List<List<Integer>> permute2(int[] nums) {
-		
-		// 过程省略
-		return null;
-	}
 	
 
 	/**
@@ -59,18 +49,57 @@ public class Permutate {
     			res.add(new_list);
     		}    		
     		permu.add(i, temp_ele);
-    	}
-    	
+    	}	
     	return res;
+    }
+    
+    
+    /**
+     * 回溯法
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute2(int[] nums) {
+        
+        // 过程省略
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        int flags [] = new int [nums.length];
+        List<Integer> route = new ArrayList<Integer>();
+
+        this.backtrack(nums, flags, res, route);
+        return res;
+    }
+    
+    private void backtrack(int [] nums, int [] flags, List<List<Integer>> res, List<Integer> route) {
+        if (nums.length == route.size()) {
+            List<Integer> temp_route = new ArrayList<Integer>();
+            for (int e : route) {
+                temp_route.add(e);
+            }
+            res.add(temp_route);
+            return;
+        }
+        
+        for (int i=0; i<flags.length; i++) {
+            if (1 == flags[i]) {
+                continue;
+            }
+            
+            route.add(nums[i]);
+            flags[i] = 1;
+            this.backtrack(nums, flags, res, route);
+            flags[i] = 0;
+            route.remove(route.size() - 1);
+        }
     }
     
     public static void main(String[] args) {
 		
     	Permutate obj = new Permutate();
     	
-    	int [] nums = new int [] {1,2,3};
+    	int [] nums = new int [] {1,2,3,4};
     	
-    	List<List<Integer>> lists = obj.permute(nums);
+    	List<List<Integer>> lists = obj.permute2(nums);
     	for (List<Integer> list : lists) {
     		for (int e : list) {
     			System.out.print(e + " ");
---
+++ src/main/java/leetcode/algs/string/package-info.java
@@ -0,0 +1,62 @@
+/******************************************************************
+ *
+ *    Java Lib For Android, Powered By Shenzhen Jiuzhou.
+ *
+ *    Copyright (c) 2001-2014 Digital Telemedia Co.,Ltd
+ *    http://www.d-telemedia.com/
+ *
+ *    Package:     leetcode.algs.string
+ *
+ *    Filename:    package-info.java
+ *
+ *    Description: TODO(用一句话描述该文件做什么)
+ *
+ *    Copyright:   Copyright (c) 2001-2014
+ *
+ *    Company:     Digital Telemedia Co.,Ltd
+ *
+ *    @author:     yxmfi
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2020年4月11日 上午11:59:55
+ *
+ *    Revision:
+ *
+ *    2020年4月11日 上午11:59:55
+ *        - first revision
+ *
+ *****************************************************************/
+/**
+ * @ClassName package-info
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @author yxmfi
+ * @Date 2020年4月11日 上午11:59:55
+ * @version 1.0.0
+ */
+package leetcode.algs.string;
+
+/**
+ * StringBuilder常用方法：
+ * 
+ * 1. 和String相互转换：
+ *     String转builder， Stringbuilder a = new Stringbuilder("hahaha");
+ *     builder转String, String b = "" + a;
+ * 2. builder 追加字符串：
+ *     builder.append(String)
+ * 3. 指定位置设置
+ *     builder.setCharAt(int i, char c)
+ * 4. 指定位置插入
+ *     builder.insert(int offset, String/char)
+ * 5. 删除
+ *     builder.delete(int startIndex, int endInder)
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+
