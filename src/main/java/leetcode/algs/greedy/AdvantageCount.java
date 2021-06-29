bc67f70e09f1a77a149d026aa2e462f379e8531a
“glorythesky” <yxmfight@hotmail.com>
2021-06-29 16:16:47 +0800

    fix

--- src/main/java/leetcode/algs/greedy/AdvantageCount.java
+++ src/main/java/leetcode/algs/greedy/AdvantageCount.java
@@ -1,6 +1,9 @@
 package leetcode.algs.greedy;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 /**
  * leetcode 870 田忌赛马
@@ -8,12 +11,12 @@ import java.util.Collections;
 public class AdvantageCount {
 
     public int[] advantageCount(int[] nums1, int[] nums2) {
-        
-        
+
+        List<Integer> list = new ArrayList<Integer>();
 //        Collections.sort();
-
-
-        return null;
+        Date date = new Date();
+        list.add(123);
+        return  null;
     }
     
 }
